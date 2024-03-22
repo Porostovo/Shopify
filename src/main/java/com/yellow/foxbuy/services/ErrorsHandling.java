88a3b232b9e310ac3e04a6c21bb269b43c1859df
Sarka Ruzickova <sarka.ruzickova@gmail.com>
2024-03-22 12:09:58 +0100

    some cleaning and refactorin code

---
+++ src/main/java/com/yellow/foxbuy/services/ErrorsHandling.java
@@ -0,0 +1,18 @@
+package com.yellow.foxbuy.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ErrorsHandling {
+    public static ResponseEntity<?> handleValidationErrors(BindingResult bindingResult) {
+        Map<String, String> result = new HashMap<>();
+        for (FieldError error : bindingResult.getFieldErrors()) {
+            result.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(400).body(result);
+    }
+}
--- src/test/java/com/yellow/foxbuy/controllers/UserControllerTest.java
+++ src/test/java/com/yellow/foxbuy/controllers/UserControllerTest.java
@@ -1,19 +1,12 @@
 package com.yellow.foxbuy.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.yellow.foxbuy.models.ConfirmationToken;
-import com.yellow.foxbuy.models.DTOs.UserDTO;
-import com.yellow.foxbuy.models.User;
-import com.yellow.foxbuy.repositories.ConfirmationTokenRepository;
-
 import com.yellow.foxbuy.models.DTOs.LoginRequest;
 import com.yellow.foxbuy.models.DTOs.UserDTO;
-
+import com.yellow.foxbuy.repositories.ConfirmationTokenRepository;
 import com.yellow.foxbuy.repositories.UserRepository;
 import com.yellow.foxbuy.services.ConfirmationTokenService;
 import com.yellow.foxbuy.services.UserService;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,15 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-
-import java.util.UUID;
-
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -266,7 +251,7 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.message", is("Username or password are incorrect.")));
     }
 
-    //    @Test
+//        @Test
 //    public void testVerificationEmailConfirmEndpoint() throws Exception {
 //        User user = new User("user", "emaile@mail.com", "Password1", false);
 //        userRepository.save(user);
