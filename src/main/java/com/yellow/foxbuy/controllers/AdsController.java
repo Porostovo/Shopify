4254356882452abb21f08bf7f694ca274bc8e5f2
Sarka Ruzickova <sarka.ruzickova@gmail.com>
2024-03-25 16:46:24 +0100

    postmapping endpoint started but need to be fixed...

--- src/main/java/com/yellow/foxbuy/controllers/AdsController.java
+++ src/main/java/com/yellow/foxbuy/controllers/AdsController.java
@@ -2,6 +2,8 @@ package com.yellow.foxbuy.controllers;
 
 import com.yellow.foxbuy.models.DTOs.AdDTO;
 import com.yellow.foxbuy.services.AdService;
+import com.yellow.foxbuy.services.ErrorsHandling;
+import com.yellow.foxbuy.utils.JwtUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdsController {
     private final AdService adService;
+    private final JwtUtil jwtUtil;
 
     @Autowired
-    public AdsController(AdService adService) {
+    public AdsController(AdService adService, JwtUtil jwtUtil) {
         this.adService = adService;
+        this.jwtUtil = jwtUtil;
     }
     @PostMapping("/advertisement")
     public ResponseEntity<?> createAd(@Valid @RequestBody AdDTO adDTO, BindingResult bindingResult){
 
+        if (bindingResult.hasErrors()) {
+            return ErrorsHandling.handleValidationErrors(bindingResult);
+        }
+//        //informations from token extraction..need to be fix
+//        final String username;
+//        final String jwt = null;
+//        username = jwtUtil.getUsernameFromJWT(jwt);
         return null;
     }
 }
