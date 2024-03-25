ccd1bc4e83f2d963631f8c1a71685eddd628540b
Sarka Ruzickova <sarka.ruzickova@gmail.com>
2024-03-25 16:34:59 +0100

    saving ads in repository method added

--- src/main/java/com/yellow/foxbuy/services/AdService.java
+++ src/main/java/com/yellow/foxbuy/services/AdService.java
@@ -1,4 +1,7 @@
 package com.yellow.foxbuy.services;
 
+import com.yellow.foxbuy.models.Ad;
+
 public interface AdService {
+    Ad saveAd (Ad ad);
 }
--- src/main/java/com/yellow/foxbuy/services/AdServiceImp.java
+++ src/main/java/com/yellow/foxbuy/services/AdServiceImp.java
@@ -1,5 +1,6 @@
 package com.yellow.foxbuy.services;
 
+import com.yellow.foxbuy.models.Ad;
 import com.yellow.foxbuy.repositories.AdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,10 @@ public class AdServiceImp implements AdService{
 @Autowired
     public AdServiceImp(AdRepository adRepository) {
         this.adRepository = adRepository;
+    }
+
+    @Override
+    public Ad saveAd(Ad ad) {
+        return adRepository.save(ad);
     }
 }
