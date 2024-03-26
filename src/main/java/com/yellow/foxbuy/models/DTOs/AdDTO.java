9f467009da9a78df24a3fdb20b8a7f740ed97842
Sarka Ruzickova <sarka.ruzickova@gmail.com>
2024-03-26 12:57:38 +0100

    just grammatical fix

--- src/main/java/com/yellow/foxbuy/models/DTOs/AdDTO.java
+++ src/main/java/com/yellow/foxbuy/models/DTOs/AdDTO.java
@@ -1,5 +1,6 @@
 package com.yellow.foxbuy.models.DTOs;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,17 +11,23 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@Schema(description = "Schema used for creating and changing ads")
 public class AdDTO {
     @NotBlank(message = "Title is required!")
+    @Schema(description = "required", example = "Leviathan Axe")
     private String title;
     @NotBlank(message = "Description is required!")
+    @Schema(description = "required", example = "Good axe to kill norse gods. Used, some scratches and blood marks.")
     private String description;
     @NotNull(message ="Price is required!")
+    @Schema(description = "required", example = "3000.00")
     private Double price;
     @NotBlank(message = "Zipcode is required!")
     @Size(min = 5, max = 5, message = "Zipcode must be 5 digits long")
+    @Schema(description = "required must have maximum 5 characters", example = "12345")
     private String zipcode;
     @NotNull(message = "Category is required!")
+    @Schema(description = "required", example = "3")
     private Long categoryID;
 
     public AdDTO(String title, String description, Double price, String zipcode, Long categoryID) {
