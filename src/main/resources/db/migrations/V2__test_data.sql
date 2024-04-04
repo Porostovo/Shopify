502293c4f6c75b4501f6b20490a7d6493a09f1bd
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-04-04 18:58:26 +0200

    First migrations created

---
+++ src/main/resources/db/migrations/V2__test_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO role VALUES
+                     (1, "ROLE_USER"),
+                     (2, "ROLE_VIP_USER"),
+                     (3, "ROLE_ADMIN");
+INSERT INTO category (name, description) VALUES
+                     ("Beverage", "Buy some good beer."),
+                     ("Nourishment", "Buy some good beef.");
+INSERT INTO user_details (id, username, email, password, verified) VALUES
+                     (UUID(), "JohnUSER", "email@email.com", "password", TRUE),
+                     (UUID(), "JohnADMIN", "emailA@email.com", "password", TRUE);
+-- INSERT INTO ad (title, description, price, zipcode, category_id, user_id)
