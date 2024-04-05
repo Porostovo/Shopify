d3c0fed873985e8a69ceee9194c2c8b20b792638
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-04-05 17:09:44 +0200

    migration fixed

--- src/main/java/com/yellow/foxbuy/FoxbuyYellowApplication.java
+++ src/main/java/com/yellow/foxbuy/FoxbuyYellowApplication.java
@@ -18,10 +18,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @SpringBootApplication
 public class FoxbuyYellowApplication implements CommandLineRunner {
@@ -45,51 +42,51 @@ public class FoxbuyYellowApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Role roleUser = roleRepository.save(new Role("ROLE_USER"));
-        Role roleVipUser = roleRepository.save(new Role("ROLE_VIP_USER"));
-        Role roleAdmin = roleRepository.save(new Role("ROLE_ADMIN"));
+//        Role roleUser = roleRepository.save(new Role("ROLE_USER"));
+//        Role roleVipUser = roleRepository.save(new Role("ROLE_VIP_USER"));
+//        Role roleAdmin = roleRepository.save(new Role("ROLE_ADMIN"));
+//
+//        Category beverageCategory = new Category("Beverage", "Buy some good beer.");
+//        Category nourishmentCategory = new Category("Nourishment", "Buy some good beef.");
+//
+//        categoryRepository.save(beverageCategory);
+//        categoryRepository.save(nourishmentCategory);
+//
+//        User user1 = new User("JohnUSER",
+//                "email@email.com",
+//                SecurityConfig.passwordEncoder().encode("password"),
+//                new HashSet<>(Collections.singletonList(roleUser)));
+//        user1.setVerified(true);
+//        userRepository.save(user1);
 
-        Category beverageCategory = new Category("Beverage", "Buy some good beer.");
-        Category nourishmentCategory = new Category("Nourishment", "Buy some good beef.");
-
-        categoryRepository.save(beverageCategory);
-        categoryRepository.save(nourishmentCategory);
-
-        User user1 = new User("JohnUSER",
-                "email@email.com",
-                SecurityConfig.passwordEncoder().encode("password"),
-                new HashSet<>(Collections.singletonList(roleUser)));
-        user1.setVerified(true);
-        userRepository.save(user1);
-
-        User user2 = new User("JohnADMIN",
-                "emailA@email.com",
-                SecurityConfig.passwordEncoder().encode("password"),
-                new HashSet<>(Collections.singletonList(roleAdmin)));
-        user2.setVerified(true);
-        userRepository.save(user2);
-
-        Ad ad = new Ad("Pilsner urquell", "Tasty beer.", 3000.00, "12345", user1, beverageCategory);
-        Ad ad1 = new Ad("Budweiser", "Good beer.", 2000.00, "23456", user1, beverageCategory);
-        adRepository.save(ad);
-        adRepository.save(ad1);
-
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, nourishmentCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
-        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        User user2 = new User("JohnADMIN",
+//                "emailA@email.com",
+//                SecurityConfig.passwordEncoder().encode("password"),
+//                new HashSet<>(Collections.singletonList(roleAdmin)));
+//        user2.setVerified(true);
+//        userRepository.save(user2);
+//
+//        Ad ad = new Ad("Pilsner urquell", "Tasty beer.", 3000.00, "12345", user1, beverageCategory);
+//        Ad ad1 = new Ad("Budweiser", "Good beer.", 2000.00, "23456", user1, beverageCategory);
+//        adRepository.save(ad);
+//        adRepository.save(ad1);
+//
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, nourishmentCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
+//        adRepository.save(new Ad("Ad1","Description1", 1000, "12345", user1, beverageCategory));
     }
 }
--- src/main/resources/db/migrations/V2__test_data.sql
+++ src/main/resources/db/migrations/V2__test_data.sql
@@ -5,7 +5,7 @@ INSERT INTO role VALUES
 INSERT INTO category (name, description) VALUES
                      ("Beverage", "Buy some good beer."),
                      ("Nourishment", "Buy some good beef.");
-INSERT INTO user_details (id, username, email, password, verified) VALUES
-                     (UUID(), "JohnUSER", "email@email.com", "password", TRUE),
-                     (UUID(), "JohnADMIN", "emailA@email.com", "password", TRUE);
+# INSERT INTO user_details (id, username, email, password, verified) VALUES
+#                      (UUID(), "JohnUSER", "email@email.com", "password", TRUE),
+#                      (UUID(), "JohnADMIN", "emailA@email.com", "password", TRUE);
 -- INSERT INTO ad (title, description, price, zipcode, category_id, user_id)
