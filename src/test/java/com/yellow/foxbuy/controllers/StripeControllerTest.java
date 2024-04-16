7cfeadc03f36cd3f33124ad46fe45d683f735001
Vojtěch Hubka <146846360+Porostovo@users.noreply.github.com>
2024-04-16 12:06:23 +0200

    Update StripeControllerTest.java
    addin missing import import org.springframework.security.test.context.support.WithMockUser;

--- src/test/java/com/yellow/foxbuy/controllers/StripeControllerTest.java
+++ src/test/java/com/yellow/foxbuy/controllers/StripeControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.List;
 
